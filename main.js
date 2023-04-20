cf18f1fecd8ff950d3af954ad2275fba3aab02cc
Kevingu08 <guidourbinakevin@gmail.com>
2023-04-19 23:46:07 -0600

    add project

---
+++ main.js
@@ -0,0 +1,60 @@
+const slider = document.querySelector("#slider");
+
+let sliderSection = document.querySelectorAll(".slider-section");
+
+let sliderSectionLast = sliderSection[sliderSection.length - 1];
+
+const btnLeft = document.querySelector("#btn-left");
+const btnRight = document.querySelector("#btn-right");
+
+slider.insertAdjacentElement("afterbegin", sliderSectionLast);
+
+
+
+function prev() {
+  let sliderSection = document.querySelectorAll(".slider-section");
+  let sliderSectionLast = sliderSection[sliderSection.length - 1];
+  slider.style.marginLeft = "0";
+  slider.style.transition = "all 0.5s";
+
+  setTimeout( function () {
+    slider.style.transition = "none";
+    slider.insertAdjacentElement("afterbegin", sliderSectionLast);
+    slider.style.marginLeft = "-100%";
+  }, 500);
+}
+
+function next() {
+  let sliderSectionFirst = document.querySelectorAll(".slider-section")[0];
+  slider.style.marginLeft = "-200%";
+  slider.style.transition = "all 0.5s";
+  setTimeout(function () {
+    slider.style.transition = "none";
+    slider.insertAdjacentElement("beforeend", sliderSectionFirst);
+    slider.style.marginLeft = "-100%";
+  }, 500);
+}
+
+btnRight.addEventListener("click", function () {
+  next();
+});
+
+btnLeft.addEventListener("click", function () {
+  prev();
+});
+
+
+
+// slider.addEventListener("touchmove", function moveToRight(){
+//     next()
+// })
+
+// slider.addEventListener("touchmove", function moveToLeft(){
+//     prev()
+// })
+
+
+
+setInterval(function(){
+    next()
+}, 5000)
